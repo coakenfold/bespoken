944d590ead0ffb1ab7a9bfc49fbd043c306e1458
Chad Oakenfold <chad@oakenfold.ca>
2022-07-23 16:47:20 -0400

    Products

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import { products, ProductInterface } from "../Products";
+import { products } from "../Products";
+import { Product } from "../components/Product";
 
-const Product = ({ product }: { product: ProductInterface }) => {
-  return (
-    <div>
-      <h1>{product.name}</h1>
-      <img src={product.image} alt={product.description} />
-    </div>
-  );
-};
 export const HomeScreen = () => {
   return (
     <section>
       <h1>Home Screen</h1>
-      <h2>MVP: can order a t-shirt</h2>
       <Row>
         {products.map((product, index) => (
           <Col sm={12} md={6} lg={4} xl={3}>
